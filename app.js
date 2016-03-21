28c56827dd860611c6371ecfb8255fe47e9e58f0
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-21 12:29:13 -0700

    bootstrapping

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+// Static assets go in resources folder
+app.use(express.static('resources'))
+
+// Use JSX and React Views
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function() {
+  console.log('Example app listening on http://localhost:3000')
+})
