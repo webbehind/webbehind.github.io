3bca31277043c8c1a0a19a63f6c845bdbe01e9d8
Jon Kuperman <jon.kuperman@gmail.com>
2017-01-25 22:44:46 -0800

    remove old site

--- episodes/episode-1/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: "Episode 1 – Mikeal Rogers",
-  date: "January 17, 2016",
-  author: "Jon Kuperman",
-  excerpt: "For our first episode, I interview Mikeal Rogers from the Node Foundation. We talk about his role at the Node Foundation, how he got his start in tech and everything in between.",
-  embed: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/252513132&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
-  content: "For our first episode, I interview Mikeal Rogers from the Node Foundation. We talk about his role at the Node Foundation, how he got his start in tech and everything in between.",
-  notes: ["Mikeal’s Twitter", "Node.js’ Twitter", "Node Help Repository", "NodeConf"]
-}
